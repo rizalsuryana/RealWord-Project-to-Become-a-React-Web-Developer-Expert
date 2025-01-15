07c54c37941cea645fd221198de7f2e911f0b5b3
rizalsuryana <rizal.suryn@gmail.com>
2025-01-15 20:34:23 +0700

    I slept wrong and now my neck hurts.

--- src/states/isPreload/action.js
+++ src/states/isPreload/action.js
@@ -1,3 +1,40 @@
+import api from '../../utils/api';
+import { setAuthUserActionCreator } from '../authUser/action';
+
 /**
  * @TODO: Define all the actions (creator) for the isPreLoad state
  */
+const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD',
+};
+
+const setIsPreloadActionCreator = (isPreLoad) => {
+  return {
+    type: ActionType.SET_IS_PRELOAD,
+    payload: {
+      isPreLoad,
+    }
+  };
+};
+
+const asyncPreloadProcess = () => {
+  return async (dispatch) => {
+    try {
+      // preload process
+      const authUser = await api.getOwnProfile();
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      // fallback process
+      dispatch(setAuthUserActionCreator(null));
+    } finally {
+      // end preload process
+      dispatch(setIsPreloadActionCreator(false));
+    }
+  };
+};
+
+export {
+  ActionType,
+  setIsPreloadActionCreator,
+  asyncPreloadProcess,
+};
