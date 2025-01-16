9c9abdafb22b0d55480ce9d0d082fe26161cb07d
rizalsuryana <rizal.suryn@gmail.com>
2025-01-16 17:25:49 +0700

    Actio  & Reducer for State users

--- src/states/users/action.js
+++ src/states/users/action.js
@@ -1,3 +1,34 @@
 /**
  * @TODO: Define all the actions (creator) for the users state
  */
+import api from '../../utils/api';
+
+const ActionType = {
+  RECEIVE_USERS: 'RECEIVE_USERS',
+};
+
+const  receiveUsersActionCreator = (users) => {
+  return {
+    type: ActionType.RECEIVE_USERS,
+    payload: {
+      users,
+    }
+  };
+};
+
+const asyncRegisterUser = ({ id, name, password }) => {
+  return async () => {
+    try {
+      await api.register({ id, name, password });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+};
+
+
+export {
+  ActionType,
+  receiveUsersActionCreator,
+  asyncRegisterUser
+};
