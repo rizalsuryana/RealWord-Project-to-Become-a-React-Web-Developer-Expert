3d5a76551a17a20800791a7bddd1cf1bc17b93eb
rizalsuryana <rizal.suryn@gmail.com>
2025-01-16 18:52:36 +0700

    Action & Reducer for State talkDetail

--- src/states/talkDetail/action.js
+++ src/states/talkDetail/action.js
@@ -1,3 +1,69 @@
 /**
  * @TODO: Define all the actions (creator) for the talkDetail state
  */
+import api from '../../utils/api';
+
+const ActionType = {
+  RECEIVE_TALK_DETAIL: 'RECEIVE_TALK_DETAIL',
+  CLEAR_TALK_DETAIL: 'CLEAR_TALK_DETAIL',
+  TOGGLE_LIKE_TALK_DETAIL: 'TOGGLE_LIKE_TALK_DETAIL',
+};
+
+const receiveTalkDetailActionCreator = (talkDetail) => {
+  return {
+    type: ActionType.RECEIVE_TALK_DETAIL,
+    payload: {
+      talkDetail,
+    }
+  };
+};
+
+const clearTalkDetailActionCreator = () => {
+  return {
+    type: ActionType.CLEAR_TALK_DETAIL,
+  };
+};
+
+const toggleLikeTalkDetailActionCreator = (userId) => {
+  return {
+    type: ActionType.TOGGLE_LIKE_TALK_DETAIL,
+    payload: {
+      userId,
+    }
+  };
+};
+
+
+const asyncReceiveTalkDetail = (talkId) => {
+  return async (dispatch) => {
+    dispatch(clearTalkDetailActionCreator());
+    try {
+      const talkDetail = await api.getTalkDetail(talkId);
+      dispatch(receiveTalkDetailActionCreator(talkDetail));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+};
+
+const asyncToggleLikeTalkDetail = () => {
+  return async (dispatch, getState) => {
+    const { authUser, talkDetail } = getState();
+    dispatch(toggleLikeTalkDetailActionCreator(authUser.id));
+
+    try {
+      await api.toggleLikeTalk(talkDetail.id);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+};
+
+export {
+  ActionType,
+  receiveTalkDetailActionCreator,
+  clearTalkDetailActionCreator,
+  toggleLikeTalkDetailActionCreator,
+  asyncReceiveTalkDetail,
+  asyncToggleLikeTalkDetail,
+};
