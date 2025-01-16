c95b82b9ec832c2ab9d8e73395291b9478641d20
rizalsuryana <rizal.suryn@gmail.com>
2025-01-16 19:01:48 +0700

    Create Share Action

--- src/states/shared/action.js
+++ src/states/shared/action.js
@@ -1,3 +1,25 @@
 /**
  * @TODO: Define all the actions (creator) that uses a combination of actions from various domain
  */
+import api from '../../utils/api';
+import { receiveTalksActionCreator } from '../talks/action';
+import { receiveUsersActionCreator } from '../users/action';
+
+const asyncPopulateUsersAndTalks = () => {
+  return async (dispatch) => {
+    try {
+      const users = await api.getAllUsers();
+      const talks = await api.getAllTalks();
+
+      dispatch(receiveUsersActionCreator(users));
+      dispatch(receiveTalksActionCreator(talks));
+    } catch (error) {
+      alert(error.message);
+
+    }
+  };
+};
+
+export {
+  asyncPopulateUsersAndTalks,
+};
