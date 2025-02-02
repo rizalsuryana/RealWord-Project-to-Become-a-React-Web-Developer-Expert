7b5d46cd130795557f2a5efbab3af4a7fb0093ec
rizalsuryana <rizal.suryn@gmail.com>
2025-02-03 03:42:31 +0700

    cypress e2e

--- eslint.config.js
+++ eslint.config.js
@@ -4,12 +4,14 @@ import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
 import pluginHooks from 'eslint-plugin-react-hooks';
 import daStyle from 'eslint-config-dicodingacademy';
+import pluginCypress from 'eslint-plugin-cypress/flat';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,jsx}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginCypress.configs.recommended,
   {
     plugins: {
       'react-hooks': fixupPluginRules(pluginHooks)
